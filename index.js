1ee1572b5b12fbea6de08ef5394521385c7258ea
Samantha Mellone <samanthamellone@gmail.com>
2018-10-09 02:16:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma (string){
   if(string === string.toLowerCase()){
     return("I can't hear you!")
   }
-  else if(string === string.toUppserCase()){
+  else if(string === string.toUpperCase()){
     return("YES INDEED!")
   }
 }
