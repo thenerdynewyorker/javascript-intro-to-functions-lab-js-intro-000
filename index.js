9e4fa8775b8c36b725fab6ea0ed57d80f9bfff3e
Samantha Mellone <samanthamellone@gmail.com>
2018-10-09 02:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  if (string = .toLowerCase()){
+    return("I can't hear you!")
+  }
+}
